bc6e0ac8c7ce29081a1531a731ca4846e3f0d986
soonitoon <soonitoon@gmail.com>
2021-02-17 11:43:38 +0900

    Add useBeforeLeave

---
+++ useBeforeLeave.js
@@ -0,0 +1,18 @@
+export const useBeforeLeave = (onLeave) => {
+  if (typeof onLeave !== "function") {
+    return;
+  }
+  const handle = (event) => {
+    const { clientY } = event;
+    if (clientY <= 0) {
+      onLeave();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mouseleave", handle);
+    return () => {
+      document.removeEventListener("mouseleave", handle);
+    };
+  }, []);
+};
