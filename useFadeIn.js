79b3f2df76c66bc498744dde4bd309d4e49b4f45
soonitoon <soonitoon@gmail.com>
2021-02-20 14:55:48 +0900

    Add useFadeIn

---
+++ useFadeIn.js
@@ -0,0 +1,14 @@
+export const useFadeIn = (duration = 2, delay = 0) => {
+  if (typeof duration !== "number" || typeof delay !== "number") {
+    return;
+  }
+  const element = useRef();
+  useEffect(() => {
+    if (element) {
+      const { current } = element;
+      current.style.transition = `opacity ${duration}s ease-in-out ${delay}s`;
+      current.style.opacity = 1;
+    }
+  }, []);
+  return { ref: element, style: { opacity: 0 } };
+};
