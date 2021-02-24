80346c9f9e58b3b6da5a64357ecb5c87fb081c60
soonitoon <soonitoon@gmail.com>
2021-02-24 13:24:32 +0900

    Add useFullscreen

---
+++ useFullscreen.js
@@ -0,0 +1,36 @@
+export const useFullscreen = (fullscreenCallback) => {
+  const element = useRef();
+  const runCallback = (isFull) => {
+    if (fullscreenCallback && typeof fullscreenCallback === "function") {
+      fullscreenCallback(isFull);
+    }
+  };
+  const onFullscreen = () => {
+    if (element.current) {
+      if (element.current.requestFullscreen) {
+        element.current.requestFullscreen();
+      } else if (element.current.mozRequestFullscreen) {
+        element.current.mozRequestFullscreen();
+      } else if (element.current.webkitRequestFullscreen) {
+        element.current.webkitRequestFullscreen();
+      } else if (element.current.msRequestFullscreen) {
+        element.current.msRequestFullscreen();
+      }
+    }
+    runCallback(true);
+  };
+  const offFullscreen = () => {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.mozCancelFullscreen) {
+      document.mozCancelFullscreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen();
+    }
+    runCallback(false);
+  };
+
+  return { element, onFullscreen, offFullscreen };
+};
