72e21f4aee6eaf367671c276bafc4d0086941c32
soonitoon <soonitoon@gmail.com>
2021-03-09 22:40:04 +0900

    Chore: Change export to export default

--- useClick/index.js
+++ useClick/index.js
@@ -1,6 +1,6 @@
 import { useRef, useEffect } from "react";
 
-export const useClick = (handleClick) => {
+const useClick = (handleClick) => {
   if (typeof handleClick !== "function") {
     return;
   }
@@ -17,3 +17,5 @@ export const useClick = (handleClick) => {
   }, []);
   return element;
 };
+
+export default useClick;
